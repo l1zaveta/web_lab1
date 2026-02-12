fb9725fc038e3da2f7977aa6c43b85b397adf8fb
MacBook <macbook@MacBook-Air.local>
2026-02-12 16:39:03 +0300

    добавлен массив товаров

---
+++ script.js
@@ -0,0 +1,60 @@
+
+const products = [
+    {
+        id: 1,
+        name: "Кольцо-цветок",
+        price: 1800,
+        description: "Доступно в розовом цвете",
+        category: "Кольца",
+        image: "images/ring1.png"
+    },
+    {
+        id: 2,
+        name: "Плетеные четки с крестом",
+        price: 3000,
+        description: "В серебряном цвете",
+        category: "Ожерелья",
+        image: "images/nec1.png"
+    },
+    {
+        id: 3,
+        name: "Плетеные четки с крестом",
+        price: 3000,
+        description: "В черном цвете",
+        category: "Ожерелья",
+        image: "images/nec2.png"
+    },
+    {
+        id: 4,
+        name: "Плетеные четки с крестом",
+        price: 3000,
+        description: "В белом цвете",
+        category: "Ожерелья",
+        image: "images/nec4.png"
+    },
+    {
+        id: 5,
+        name: "Брелок 'Сакура'",
+        price: 2500,
+        description: "Белый цветок + дерево",
+        category: "Брелоки",
+        image: "images/thing.png"
+    },
+    {
+        id: 6,
+        name: "Чокер с цветами",
+        price: 3500,
+        description: "Доступен в розовом цвете",
+        category: "Ожерелья",
+        image: "images/nec3.png"
+    },
+    {
+        id: 7,
+        name: "Кольцо 'Sparkle'",
+        price: 2000,
+        description: "В белом цвете",
+        category: "Кольца",
+        image: "images/ring2.png"
+    },
+    
+];
